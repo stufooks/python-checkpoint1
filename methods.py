f44e5a16f10e442637a4f9d36cea79e8e11ee37b
Stuart Fooks <sb.fooks@gmail.com>
2019-03-04 09:37:16 -0500

    oop mostly passing

--- methods.py
+++ methods.py
@@ -5,7 +5,11 @@
 # returns a list of integers between 1 and the number passed in.
 #
 # So if the number 5 is passed in, num_list_with_arg should return [1, 2, 3, 4]
-
+def num_list_with_arg(int):
+    myList = []
+    for num in range(1,int):
+        myList.append(num)
+    return myList
 
 # #2: Modify the has_ruby_exp method below so that it returns a SORTED list of
 # all instructors who have Ruby experience (i.e. where "ruby" == True)
@@ -49,6 +53,8 @@ def has_ruby_exp():
             'pasta': False
         }
     }
+
+
     
 
 
@@ -58,5 +64,12 @@ def has_ruby_exp():
 # argument is neither a string nor an integer, return the string "this is not a
 # str or a int":
 
+def toggle_str_num(arg):
+    if type(arg) == str:
+        return int(arg)
+    elif type(arg) == int:
+        return str(arg)
+    else:
+        return "this is not a str or a int"
 
 # Commit when you finish working on these questions!
--- oop.py
+++ oop.py
@@ -17,6 +17,27 @@
 #
 # Your Vehicle class should take one argument (a `dict`) with the above
 # attributes. Define the properties on the class from the dict that is passed in.
+class Vehicle:
+    def __init__(self, dct):
+        self.vehicle_type = dct["vehicle_type"]
+        self.wheel_count = dct["wheel_count"]
+        self.name = dct["name"]
+        self.cost = dct["cost"]
+        self.colors = dct["colors"]
+        self.vehicle_brand = dct["vehicle_brand"]
+        self.mpg = dct["mpg"]
+    
+    def get_vehicle_type(self):
+        return self.vehicle_type
+    
+    def get_vehicle_brand(self):
+        return self.vehicle_brand
+    
+    def get_vehicle_drive(self):
+        if self.wheel_count == "no wheels!":
+            return "no wheels send it back to the shop"
+        else:
+            return "I have "  + self.wheel_count  + " wheel drive"
 
 
 
@@ -26,18 +47,42 @@
 # - method: `pop_wheelie` if `wheel_count` is not equal to 2 then it should be False,
 #       otherwise return "......pop!"
 
+class Motorcycle(Vehicle):
+    def __init__(self, dct):
+        super().__init__(dct)
+        self.wheel_count = "no wheels!"
+    
+    def pop_wheelie(self):
+        if self.wheel_count != 2:
+            return False
+        else:
+            return "......pop!"
 
 
 # #3: Define a Car class that inherits from the vehicle class with the following attributes and methods:
 # - property: `wheel_count` defaults to 4
 # - method: `can_drive` that should return 'Vrrooooom Vroooom'
 
+class Car(Vehicle):
+    def __init__(self, dct):
+        super().__init__(dct)
+        self.wheel_count = 4
+
+    def can_drive(self):
+        return "Vrrooooom Vroooom"
 
 
 # #4: Define a Truck class that inherits from the vehicle class with the following attributes and methods:
 # - property: `wheel_count` defaults to "no wheels!"
 # - method: `rev_engine` that should return 'revvvvvreeeev'
 
+class Truck(Vehicle):
+    def __init__(self, dct):
+        super().__init__(dct)
+        self.wheel_count = "no wheels!"
+    
+    def rev_engine(self):
+        return "revvvvvreeeev"
 
 
 # Commit when you finish working on these questions!
